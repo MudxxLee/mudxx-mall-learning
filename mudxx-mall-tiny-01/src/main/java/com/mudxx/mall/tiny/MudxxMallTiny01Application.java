98e3dbb6a87ba0cde3e1aa38f99163a93af58684
laiw <laiw@agilewing.net>
2023-02-07 18:07:23 +0800

    基础

---
+++ mudxx-mall-tiny-01/src/main/java/com/mudxx/mall/tiny/MudxxMallTiny01Application.java
@@ -0,0 +1,13 @@
+package com.mudxx.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudxxMallTiny01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudxxMallTiny01Application.class, args);
+    }
+
+}
---
+++ mudxx-mall-tiny-01/src/test/java/com/mudxx/mall/tiny/MudxxMallTiny01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudxx.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudxxMallTiny01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
