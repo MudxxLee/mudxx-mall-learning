ba6d8dbce29ccfc876cc2d1cd6061d3544e7ddf5
laiw <laiw@agilewing.net>
2023-02-10 16:27:45 +0800

    SpringTask基础

---
+++ mudxx-mall-tiny-01/src/main/java/com/mudxx/mall/tiny/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,28 @@
+package com.mudxx.mall.tiny.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 订单超时取消并解锁库存的定时器
+ * @author laiw
+ * @date 2023/2/10 16:23
+ */
+@Component
+public class OrderTimeOutCancelTask {
+    private final Logger logger = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+
+    /**
+     * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
+     * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
+     */
+    @Scheduled(cron = "0/10 * * * * ? ")
+    private void cancelTimeOutOrder() {
+        // TODO: 2019/5/3 此处应调用取消订单的方法，具体查看mall项目源码
+        logger.info("取消订单，并根据sku编号释放锁定库存");
+    }
+
+
+}
---
+++ mudxx-mall-tiny-01/src/main/java/com/mudxx/mall/tiny/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package com.mudxx.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author laiw
+ * @date 2023/2/10 16:22
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
